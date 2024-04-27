7a07bdf16b06d4610fad4910b59c7036acb437b9
Brian Fontillas <BrianKFontillas@gmail.com>
2024-04-27 13:05:11 -0700

    add list functionality (update and delete)

--- todos.js
+++ todos.js
@@ -4,6 +4,7 @@ const flash = require("express-flash");
 const session = require("express-session");
 const { body, validationResult } = require("express-validator");
 const TodoList = require("./lib/todolist");
+const { sortTodoLists, sortTodos } = require("./lib/sort");
 
 const app = express();
 const host = "localhost";
@@ -31,27 +32,17 @@ app.use((req, res, next) => {
   next();
 });
 
-// Compare todo list titles alphabetically
-const compareByTitle = (todoListA, todoListB) => {
-  let titleA = todoListA.title.toLowerCase();
-  let titleB = todoListB.title.toLowerCase();
-
-  if (titleA < titleB) {
-    return -1;
-  } else if (titleA > titleB) {
-    return 1;
-  } else {
-    return 0;
-  }
+// Find a todo list with the indicated ID. Returns `undefined` if not found.
+const loadToDoList = todoListId => {
+  return todoLists.find(todoList => todoList.id === todoListId);
 };
 
-// return the list of todo lists sorted by completion status and title.
-const sortTodoLists = lists => {
-  let undone = lists.filter(todoList => !todoList.isDone());
-  let done   = lists.filter(todoList => todoList.isDone());
-  undone.sort(compareByTitle);
-  done.sort(compareByTitle);
-  return [].concat(undone, done);
+const toggleTodo = todo => {
+  if (todo.isDone()) {
+    todo.markUndone();
+  } else {
+    todo.markDone();
+  }
 };
 
 app.get("/", (req, res) => {
@@ -98,6 +89,63 @@ app.post("/lists",
     }
   }
 );
+
+app.get("/lists/:todoListId", (req, res, next) => {
+  let todoListId = req.params.todoListId;
+  let todoList = loadToDoList(+todoListId);
+
+  if (todoList === undefined) {
+    next(new Error("Not found."));
+  } else {
+    res.render("list", {
+      todoList: todoList,
+      todos: sortTodos(todoList),
+    });
+  }
+});
+
+app.post("/lists/:todoListId/todos/:todoId/toggle", (req, res, next) => {
+  let { todoListId, todoId } = { ...req.params };
+  let todoList = loadToDoList(+todoListId);
+  let todo = todoList.findById(+todoId);
+
+  if (!todo) {
+    next(new Error("Not found."));
+  } else {
+    let title = todo.title;
+    if (todo.isDone()) {
+      todo.markUndone();
+      req.flash("success", `"${title}" marked as NOT done!`);
+    } else {
+      todo.markDone();
+      req.flash("success", `"${title}" marked as done.`);
+    }
+  }
+
+  res.redirect(`/lists/${todoListId}`);
+});
+
+app.post("/lists/:todoListId/todos/:todoId/destroy", (req, res, next) => {
+  let { todoListId, todoId } = { ...req.params };
+  let todoList = loadToDoList(+todoListId);
+  let todo = todoList.findById(+todoId);
+
+  if(!todo) {
+    next(new Error("Not found."));
+  } else {
+    let title = todo.title;
+    let todoToRemoveIndex = todoList.findIndexOf(todo);
+    todoList.removeAt(todoToRemoveIndex);
+    req.flash("success", `"${title} has been deleted!"`);
+  }
+
+  res.redirect(`/lists/${todoListId}`);
+});
+
+app.use((err, req, res, _next) => {
+  console.log(err);
+  res.status(404).send(err.message);
+});
 
 //listener
 app.listen(port, host, () => {
